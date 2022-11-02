bb8e7ee83329b4439819a736708fa1c296676e4f
Lyserith <Lyserith@pm.me>
2022-11-02 14:50:20 +0100

    updates

--- src/commands/balance.rs
+++ src/commands/balance.rs
@@ -1,4 +1,5 @@
 use pea_api::get;
+use pea_core::constants::DECIMAL_PRECISION;
 use serenity::{
     builder::CreateApplicationCommand,
     model::{
@@ -12,11 +13,11 @@ const HTTP_API: &str = "http://localhost:8080";
 pub async fn run(ctx: &Context, command: &ApplicationCommandInteraction) {
     if let CommandDataOptionValue::String(address) = command.data.options.get(0).expect("Expected address option").resolved.as_ref().expect("Expected address object") {
         let balance = match get::balance(HTTP_API, address).await {
-            Ok(a) => a.to_string(),
+            Ok(a) => (a as f64 / DECIMAL_PRECISION as f64).to_string(),
             Err(_) => "Unknown".to_string(),
         };
         let balance_staked = match get::balance_staked(HTTP_API, address).await {
-            Ok(a) => a.to_string(),
+            Ok(a) => (a as f64 / DECIMAL_PRECISION as f64).to_string(),
             Err(_) => "Unknown".to_string(),
         };
         if let Err(why) = command
